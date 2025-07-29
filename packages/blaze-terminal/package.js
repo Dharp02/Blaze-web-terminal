e717bb716b7c673de4ab6d24b662b1e27b4525ac
pfw_poonam <dharp02@pfw.edu>
2025-07-28 22:15:04 -0400

    Created blaze package

---
+++ packages/blaze-terminal/package.js
@@ -0,0 +1,40 @@
+Package.describe({
+  name: 'blaze-terminal',
+  version: '0.0.1',
+  summary: 'Terminal component for Meteor with Blaze',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('3.3');
+  api.use(['ecmascript', 'templating@1.4.2', 'reactive-var', 'random'], 'client');
+  
+  api.addFiles([
+    'client/terminal.html',
+    'client/terminal.css'
+  ], 'client');
+  
+  api.mainModule('client/terminal.js', 'client');
+
+
+  
+  api.use('ecmascript', 'server');
+  api.mainModule('server/main.js', 'server');
+
+
+   Npm.depends({
+    "ssh2": "1.16.0",
+    "ws": "8.18.3",
+    "xterm": "5.3.0",
+    "xterm-addon-fit": "0.8.0"
+    });
+
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('blaze-terminal');
+  api.mainModule('blaze-terminal-tests.js');
+});
